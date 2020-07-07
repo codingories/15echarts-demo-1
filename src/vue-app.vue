616ce00905d9140ed9cb63217a0b43b98cd24593
朱骏杰 <ories.zhu@mochain.vip>
2020-07-07 23:54:02 +0800

    初步完成Vue封装echarts

--- src/vue-app.vue
+++ src/vue-app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>如何在vue中使用echarts</h1>
-    <vue-echarts></vue-echarts>
+    <vue-echarts :option="option"></vue-echarts>
   </div>
 
 </template>
@@ -9,6 +9,35 @@
 <script>
   import VueEcharts from './vue-echarts.vue'
   export default {
+    data(){
+      return {
+        option: {
+          title: {
+            show: true,
+            text: '销量',
+            right: 30,
+          },
+          legend: {
+            data: ['金额']
+          },
+          xAxis: {
+            type: 'category',
+            data: ['2020-1-1','2020-1-2']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          tooltip: {
+            show: true,
+          },
+          series: [{
+            name: '金额',
+            data: [1,2],
+            type: 'line'
+          }]
+        }
+      }
+    },
     components: {VueEcharts}
   }
 </script>
--- src/vue-echarts.vue
+++ src/vue-echarts.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    vue echarts
+  <div ref="container">
   </div>
 </template>
+<script>
+  import echarts from 'echarts'
+  export default {
+    props: ['option'],
+
+    mounted(){
+      console.log(this.$refs.container)
+      const width = document.documentElement.clientWidth
+      this.$refs.container.style.width = `${width-20}px`
+      this.$refs.container.style.height = `${(width -20) * 1.2}px`
+      this.chart = echarts.init(this.$refs.container, 'dark')
+      this.chart.setOption(this.option)
+    }
+  }
+</script>
