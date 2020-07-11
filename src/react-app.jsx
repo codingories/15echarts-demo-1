3af41c796c5796b2e7f9416a356b94709ff48bc4
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 09:34:45 +0800

    显示echart完成

--- src/react-app.jsx
+++ src/react-app.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import {ReactEcharts} from "./react-echarts"
 
 export function ReactApp() {
+  const [option, setOption] = useState(
+    {
+      title: {
+        show: true,
+        text: '销量',
+        right: 30,
+      },
+      legend: {
+        data: ['金额']
+      },
+      xAxis: {
+        type: 'category',
+        data: ['2020-1-1','2020-1-2']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      tooltip: {
+        show: true,
+      },
+      series: [{
+        name: '金额',
+        data: [1,2],
+        type: 'line'
+      }]
+    }
+  )
   return (
     <div>
       <h1>我来在React里面使用Echarts</h1>
-      <ReactEcharts/>
+      <ReactEcharts option={option}/>
     </div>
   )
 }
--- src/react-echarts.jsx
+++ src/react-echarts.jsx
@@ -1,14 +1,20 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef,props} from 'react'
 import echarts from 'echarts'
 
-export function ReactEcharts(){
+export function ReactEcharts(props){
   const container = useRef(null)
   const chart = useRef(null)
+  const {option} = props
   useEffect(()=>{
     const width = document.documentElement.clientWidth
     container.current.style.width = `${width-20}px`
     container.current.style.height = `${(width -20) * 1.2}px`
     chart.current = echarts.init(container.current, 'dark')
   },[]) // mounted 第一次渲染, 空数组第一次渲染，不是空数组每次渲染
+
+  useEffect(()=>{
+    chart.current.setOption(option)
+  },[option]) // when option changes
+
   return (<div ref={container}/>)
 }
