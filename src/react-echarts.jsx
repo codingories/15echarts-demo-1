623a308e027e9680bdf7c09c45262391313fd664
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 09:14:20 +0800

    初始化chart

--- src/react-echarts.jsx
+++ src/react-echarts.jsx
@@ -3,8 +3,12 @@ import echarts from 'echarts'
 
 export function ReactEcharts(){
   const container = useRef(null)
+  const chart = useRef(null)
   useEffect(()=>{
-    console.log(container.current)
+    const width = document.documentElement.clientWidth
+    container.current.style.width = `${width-20}px`
+    container.current.style.height = `${(width -20) * 1.2}px`
+    chart.current = echarts.init(container.current, 'dark')
   },[]) // mounted 第一次渲染, 空数组第一次渲染，不是空数组每次渲染
-  return (<div ref={container}>hello</div>)
+  return (<div ref={container}/>)
 }
