16cb95b60c1f48b88d55d30214c4fab97fcba019
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 08:36:42 +0800

    react版本初步搭建完成

---
+++ src/react-app.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export function ReactApp() {
+  return (
+    <div>hi</div>
+  )
+}
---
+++ src/react-main.jsx
@@ -0,0 +1,14 @@
+/**
+ * Author      :ories
+ * Create_Date :2020-07-11 08:22
+ * Description : react e-charts封装
+ * IDE         :WebStorm
+ *
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {ReactApp} from "./react-app"
+
+
+ReactDOM.render(<ReactApp/>, document.querySelector('#main'))
